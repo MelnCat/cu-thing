bda77da28f3f31ba12ad58f34a245f2eb2c0a665
MelnCat <melncatuwu@gmail.com>
2026-06-22 20:19:15 -0700

    More work

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ let config = {
 	int: 10,
 };
 
+const fibrillationrate = 1;
+
 let state = {
 	heartRate: 70,
 	fibrillationProgress: 0,
@@ -17,6 +19,8 @@ let state = {
 	heartProg: 0,
 	itemCharge: 1,
 	bloodPressure: 89,
+    respiratoryRate: 100,
+    bloodViscosity: 0,
 
 	charge: 0,
 	desiredCharge: 100,
@@ -24,6 +28,12 @@ let state = {
 	rangeOffset: Math.random() * 2 - 1,
 	bloodOxygen: 100,
 	dialAngle: 0,
+	charging: false,
+	inCardiacArrest: false,
+    fibrillationRising: false,
+    fibrillationForced: false,
+    heartRatePressureOffset: 0,
+    bloodVesselSize: 1
 };
 
 const canvas = document.getElementById("main-canvas")! as HTMLCanvasElement;
@@ -44,6 +54,14 @@ const loadImage = (src: string): Promise<HTMLImageElement> => {
 
 // lerp lerp lerp sahur
 const lerp = (a: number, b: number, t: number) => a * (1 - t) + t * b;
+const clamp = (n: number, min: number, max: number) => Math.min(max, Math.max(min, n))
+
+const moveTowards = (current: number, target: number, maxDelta: number) => {
+	if (Math.abs(target - current) <= maxDelta) {
+		return target;
+	}
+	return current + Math.sign(target - current) * maxDelta;
+};
 
 const defibBackground = await loadImage("./assets/defibBackground.png");
 const dialImage = await loadImage("./assets/manualDefibDial.png");
@@ -51,15 +69,71 @@ const chargeButtonImage = await loadImage("./assets/manualDefibCharge.png");
 const chargingImage = await loadImage("./assets/manualDefibCharging.png");
 const shockButtonImage = await loadImage("./assets/manualDefibShock.png");
 
+interface ClickableComponent {
+	x: number;
+	y: number;
+	width: number;
+	height: number;
+	isIn(x: number, y: number): boolean;
+	image: HTMLImageElement;
+	onClick(): void;
+}
+
+const components: ClickableComponent[] = [
+	{
+		x: 112,
+		y: 160,
+		width: chargeButtonImage.width,
+		height: chargeButtonImage.height,
+		image: chargeButtonImage,
+		onClick() {
+			state.charging = !state.charging;
+		},
+		isIn(x, y) {
+			return x >= this.x && x <= this.x + this.width && y >= this.y && y <= this.y + this.height;
+		},
+	},
+	{
+		x: 188,
+		y: 160,
+		width: shockButtonImage.width,
+		height: shockButtonImage.height,
+		image: shockButtonImage,
+		onClick() {
+			if (state.charge < 10) return;
+			state.shockCount++;
+			const chance = 1 - Math.abs(state.fibrillationProgress - state.charge * 0.5) / 40;
+			if (Math.random() < chance || state.inCardiacArrest) {
+				state.fibrillationProgress = 0;
+				if (state.inCardiacArrest && Math.random() < 0.2) {
+					state.heartRate = 200;
+				}
+			}
+			state.defibShockedFrames = 20;
+			state.heartProg = -1;
+			state.bloodPressure = 40;
+			//state.itemCharge -= state.charge / 4000 / (1 * 0.01);
+			state.charge = 0;
+			state.charging = false;
+		},
+		isIn(x, y) {
+			return x >= this.x && x <= this.x + this.width && y >= this.y && y <= this.y + this.height;
+		},
+	},
+];
+
 const drawBase = () => {
 	ctx.drawImage(defibBackground, 0, 0, defibBackground.width * SCALE, defibBackground.height * SCALE);
-	ctx.drawImage(chargeButtonImage, 112 * SCALE, 160 * SCALE, chargeButtonImage.width * SCALE, chargeButtonImage.height * SCALE);
-	ctx.drawImage(shockButtonImage, 188 * SCALE, 160 * SCALE, chargeButtonImage.width * SCALE, chargeButtonImage.height * SCALE);
+	for (const component of components) {
+		ctx.drawImage(component.image, component.x * SCALE, component.y * SCALE, component.width * SCALE, component.height * SCALE);
+	}
 
 	ctx.fillStyle = "#ffffff";
 	ctx.font = '42px "Retro Gaming"';
 	ctx.fillText(state.shockCount.toString(), 50 * SCALE, 53 * SCALE);
-	ctx.fillText(state.charge.toString(), 103 * SCALE, 53 * SCALE);
+	ctx.textAlign = "right";
+	ctx.fillText(Math.round(state.charge).toString(), 110 * SCALE, 53 * SCALE);
+	ctx.textAlign = "left";
 	ctx.fillStyle = "#707070";
 	ctx.fillText(Math.round(state.desiredCharge).toString(), 123 * SCALE, 53 * SCALE);
 	ctx.fillStyle = "#ffffff";
@@ -119,45 +193,194 @@ const drawBase = () => {
 };
 
 const tickCirculation = (delta: number) => {
-	// let targetHeartRate = 70;
-	// targetHeartRate += state.fibrillationProgress;
-	// if (state.fibrillationProgress > 75) {
-	// 	targetHeartRate += (state.fibrillationProgress - 75) * 4;
-	// }
-	// if (state.fibrillationProgress > 95) {
-	// 	targetHeartRate += (state.fibrillationProgress - 95) * 30;
-	// }
-	// state.heartRate = lerp(state.heartRate, targetHeartRate, delta * 0.15);
+        state.bloodOxygen += delta * (state.respiratoryRate * 0.01 - 0.5);
+		let num = 100 - Math.abs(moveTowards(state.bloodViscosity, 0, 40)) * 0.4;
+		if (state.bloodOxygen > num)
+		{
+			state.bloodOxygen = moveTowards(state.bloodOxygen, num, delta * 0.75);
+		}
+		if (state.bloodOxygen > 100 * 0.3)
+		{
+			state.bloodOxygen = moveTowards(state.bloodOxygen, 100 * 0.3, delta * 0.8);
+		}
+		state.bloodOxygen = clamp(state.bloodOxygen, 0, 100);
+		state.bloodViscosity = moveTowards(state.bloodViscosity, 0, delta * 0.05);
+		state.bloodViscosity = clamp(state.bloodViscosity, -100, 100);
+		const num2 = Math.max(0, state.bloodViscosity);
 
-	state.heartProg += (delta * state.heartRate) / 60;
-	if (state.heartProg > 1) {
-		if (state.heartProg > 1.2) {
-			state.heartProg = 1.2;
+		let num3 = 100;
+		if (state.bloodPressure > 145)
+		{
+			num3 -= state.bloodPressure - 145;
 		}
-		state.heartProg -= 1;
-		if (state.fibrillationProgress > 40) {
-			const num16 = (state.fibrillationProgress - 40) / 150;
-			state.randomFibrillationVariation = 1 + Math.random() * (2 * num16) - num16;
-		} else {
-			state.randomFibrillationVariation = 1;
+		if (state.bloodPressure < 20)
+		{
+			num3 = 0;
 		}
+		num3 -= state.fibrillationProgress * 0.35;
+		state.respiratoryRate = moveTowards(state.respiratoryRate, num3, delta * ((state.respiratoryRate > 10) ? 8 : 1));
+		state.respiratoryRate = clamp(state.respiratoryRate, 0, 100);
+		if (state.fibrillationProgress > 0)
+		{
+			if (state.fibrillationRising)
+			{
+				state.fibrillationProgress += delta * fibrillationrate;
+				if (state.heartRate > 280)
+				{
+					state.fibrillationProgress += delta * 3 * fibrillationrate;
+				}
+			}
+			else
+			{
+				state.fibrillationProgress -= delta * 0.75;
+				if (state.fibrillationProgress < 0)
+				{
+					state.fibrillationProgress = 0;
+				}
+			}
+		}
+		else
+		{
+			state.fibrillationForced = false;
+		}
+		if (state.bloodOxygen < 50 || state.bloodPressure < 78 || state.heartRate > 200 || num2 > 95)
+		{
+            if (state.fibrillationProgress <= 0) {
+                state.fibrillationProgress = 0.1;
+            }
+		}
+		if (state.fibrillationProgress >= 100)
+		{
+			state.fibrillationForced = false;
+			state.heartRate = 0;
+		}
+		state.fibrillationProgress = clamp(state.fibrillationProgress, 0, 100);
+		let num4 = 120;
+		if (!state.inCardiacArrest)
+		{
+			let targetHeartRate = 70;
+			targetHeartRate -= num2 * 0.3;
+			if (state.bloodPressure < num4 - 5)
+			{
+				state.heartRatePressureOffset += delta * 1.5;
+			}
+			if (state.bloodPressure > num4 + 5)
+			{
+				state.heartRatePressureOffset -= delta * 1.5;
+			}
+			state.heartRatePressureOffset = clamp(state.heartRatePressureOffset, -30, 80);
+			targetHeartRate += state.heartRatePressureOffset;
+			targetHeartRate += state.fibrillationProgress;
+			if (state.fibrillationProgress > 75)
+			{
+				targetHeartRate += (state.fibrillationProgress - 75) * 4;
+			}
+			if (state.fibrillationProgress > 95)
+			{
+				targetHeartRate += (state.fibrillationProgress - 95) * 30;
+			}
+			state.heartRate = lerp(state.heartRate, targetHeartRate, delta * 0.15);
+		}
+		else
+		{
+			state.heartRate = 0;
+		}
+		if (state.bloodPressure > num4 + 10)
+		{
+			state.bloodVesselSize += delta * 0.0036;
+		}
+		else if (state.bloodPressure < num4 - 10)
+		{
+			state.bloodVesselSize -= delta * 0.0036;
+		}
+		else
+		{
+			state.bloodVesselSize = moveTowards(state.bloodVesselSize, 1, delta * 0.0036);
+		}
+		state.bloodVesselSize = clamp(state.bloodVesselSize, 0.85, 1.15);
+		let num6 = clamp(state.heartRate, 0, 215) - 70;
+		num6 = ((!(num6 > 0)) ? (num6 / 70) : (num6 / 200));
+		let num7 = 1;
+		let num8 = 1 - state.fibrillationProgress / 260;
+		let num9 = 1 + state.bloodViscosity / 200;
+		let num10 = 1;
+		let num11 = 1;
+		let num12 = 1;
+		let num13 = 1;
+		let num14 = 120 * (1 + num6) * num7 * num8 * num9 * 1 * num10 * num11 * num12 * num13 / state.bloodVesselSize;
+		state.bloodPressure = lerp(state.bloodPressure, num14, delta * 0.25);
+		state.bloodPressure = clamp(state.bloodPressure, 0, 250);
+		state.heartProg += delta * state.heartRate / 60;
+		if (state.heartProg > 1)
+		{
+			if (state.heartProg > 1.2)
+			{
+				state.heartProg = 1.2;
+			}
+			state.heartProg -= 1;
+			if (state.fibrillationProgress > 40)
+			{
+				let num16 = (state.fibrillationProgress - 40) / 150;
+				state.randomFibrillationVariation = 1 + Math.random() * num16 - num16;
+			}
+			else
+			{
+				state.randomFibrillationVariation = 1;
+			}
+		}
+		state.defibShockedFrames--;
+		if (state.defibShockedFrames < 0)
+		{
+			state.defibShockedFrames = 0;
+		}
+};
+
+const tickCharge = (delta: number) => {
+	if (state.charging && state.charge < state.desiredCharge) {
+		state.charge = moveTowards(state.charge, state.desiredCharge, delta * 40);
+	}
+	if (state.charging && state.charge >= state.desiredCharge) {
+		state.charging = false;
 	}
 };
 
 let mouseX = 0;
 let mouseY = 0;
+let mouseDown = false;
+let draggingDial = false;
+let justClicked = false;
+document.body.addEventListener("mousedown", () => {
+	mouseDown = true;
+	justClicked = true;
+});
+document.body.addEventListener("mouseup", () => {
+	mouseDown = false;
+});
 document.body.addEventListener("mousemove", e => {
 	mouseX = e.clientX;
 	mouseY = e.clientY;
 });
 
 const tickControls = () => {
+	checkDial();
+	checkComponents();
+	justClicked = false;
+};
+const checkDial = () => {
+	if (!mouseDown) {
+		draggingDial = false;
+	}
 	const dialCenterX = 28 + dialImage.width / 2;
 	const dialCenterY = 154 + dialImage.height / 2;
 	const canvasBounds = canvas.getBoundingClientRect();
+	const radius = (canvasBounds.width * dialImage.width) / WIDTH / 2;
 	const centerX = canvasBounds.left + canvasBounds.width * (dialCenterX / WIDTH);
 	const centerY = canvasBounds.top + canvasBounds.height * (dialCenterY / HEIGHT);
+	if (justClicked && !draggingDial && mouseDown && (centerX - mouseX) ** 2 + (centerY - mouseY) ** 2 < radius ** 2) {
+		draggingDial = true;
+	}
 
+	if (!draggingDial) return;
 	const angle = Math.atan2(mouseY - centerY, mouseX - centerX);
 	let displayAngle = angle + Math.PI / 2;
 	if (displayAngle > Math.PI) {
@@ -166,6 +389,18 @@ const tickControls = () => {
 	displayAngle = Math.min((170 * Math.PI) / 180, Math.max((-170 * Math.PI) / 180, displayAngle));
 	state.dialAngle = displayAngle;
 	state.desiredCharge = lerp(10, 200, (displayAngle + (170 * Math.PI) / 180) / ((2 * 170 * Math.PI) / 180));
+};
+
+const checkComponents = () => {
+	if (!justClicked) return;
+	const canvasBounds = canvas.getBoundingClientRect();
+	const relativeX = ((mouseX - canvasBounds.left) * WIDTH) / canvasBounds.width;
+	const relativeY = ((mouseY - canvasBounds.top) * HEIGHT) / canvasBounds.height;
+	for (const component of components) {
+		if (component.isIn(relativeX, relativeY)) {
+			component.onClick();
+		}
+	}
 };
 
 class EcgController {
@@ -278,8 +513,11 @@ setInterval(() => {
 	}
 	const delta = (Date.now() - lastT) / 1000;
 	tickCirculation(delta);
+	tickCharge(delta);
 	tickControls();
 	drawAll(delta);
 	lastT = Date.now();
 }, 1000 / 60);
 drawBase();
+
+window.state = state;
